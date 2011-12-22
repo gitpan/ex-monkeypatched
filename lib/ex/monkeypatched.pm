c6ae03cb5841ac4be725f2c4bbc056d774a4d0a5
Aaron Crane <cpan@aaroncrane.co.uk>
2011-12-22 16:48:24 +0000

    Import of ARC/ex-monkeypatched-0.02 from CPAN.
    gitpan-cpan-distribution: ex-monkeypatched
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         ARC/ex-monkeypatched-0.02.tar.gz
    gitpan-cpan-author:       ARC
    gitpan-cpan-maturity:     released

--- lib/ex/monkeypatched.pm
+++ lib/ex/monkeypatched.pm
@@ -6,31 +6,74 @@ use warnings;
 use Sub::Name qw<subname>;
 use Carp qw<croak>;
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 sub import {
     my $invocant = shift;
+    my $norequire = @_ && $_[0] && $_[0] eq '-norequire' && shift;
     if (@_) {
-        my ($target, %routines) = @_;
-        croak qq[Invalid class name "$target"]
-            if $target !~ /\A (?!\d) \w+ (?: :: \w+ )* \z/xms;
-        eval "CORE::require $target; 1" or die $@;
-        $invocant->inject($target => %routines);
+        my @injections = _parse_injections(@_)
+            or croak "Usage: use $invocant \$class => %methods
+or:    use $invocant (class => \$class, methods => \\%methods)
+or:    use $invocant (method => \$name, implementations => \\%impl)";
+        _require(map { $_->[0] } @injections)
+            if !$norequire;
+        _inject_methods(@injections);
     }
+}
+
+sub _require {
+    for (@_) {
+        (my $as_file = $_) =~ s{::|'}{/}g;
+        require "$as_file.pm";  # dies if no such file is found
+    }
+}
+
+sub _parse_injections {
+
+    if (@_ == 1 && ref $_[0] eq 'HASH') {
+        my $opt = shift;
+        if (defined $opt->{class} && ref $opt->{methods} eq 'HASH') {
+            return map { [$opt->{class}, $_, $opt->{methods}{$_}] }
+                keys %{ $opt->{methods} };
+        }
+        elsif (defined $opt->{method} && ref $opt->{implementations} eq 'HASH') {
+            return map { [$_, $opt->{method}, $opt->{implementations}{$_}] }
+                keys %{ $opt->{implementations} };
+        }
+    }
+    elsif (@_ % 2) {
+        my @injections;
+        my $target = shift;
+        push @injections, [$target, splice @_, 0, 2]
+            while @_;
+        return @injections;
+    }
+
+    return;
 }
 
 sub inject {
-    my ($invocant, $target, %routines) = @_;
-    while (my ($name, $code) = each %routines) {
+    my $invocant = shift;
+    my @injections = _parse_injections(@_)
+        or croak "Usage: $invocant->inject(\$class, %methods)
+or:    $invocant->inject({ class => \$class, methods => \\%methods })
+or:    $invocant->inject({ method => \$name, implementations => \\%impl })";
+    _inject_methods(@injections);
+}
+
+sub _inject_methods {
+    for (@_) {
+        my ($target, $name, undef) = @$_;
         croak qq[Can't monkey-patch: $target already has a method "$name"]
             if $target->can($name);
-        _install_subroutine($target, $name, $code);
     }
+    _install_subroutine(@$_) for @_;
 }
 
 sub _install_subroutine {
-    my ($package, $name, $code) = @_;
-    my $full_name = "$package\::$name";
+    my ($target, $name, $code) = @_;
+    my $full_name = "$target\::$name";
     my $renamed_code = subname($full_name, $code);
     no strict qw<refs>;
     *$full_name = $renamed_code;
@@ -48,6 +91,12 @@ ex::monkeypatched - Experimental API for safe monkey-patching
     use ex::monkeypatched 'Third::Party::Class' => (
         clunk => sub { ... },
         eth   => sub { ... },
+    );
+
+    use Foo::TopLevel; # provides Foo::Bar, which isn't a module
+    use ex::monkeypatched -norequire => 'Foo::Bar' => (
+        thwapp => sub { ... },
+        urkk   => sub { ... },
     );
 
 =head1 BACKGROUND
@@ -84,25 +133,83 @@ details.
 
 =head1 DESCRIPTION
 
-C<ex::monkeypatched> injects methods when you C<use> it.  Your C<use> call
-should supply the name of a class to patch, and a hash from method names to
-code references implementing those methods.  The class to be patched will be
-loaded automatically before any patching is done (thus ensuring that all its
-base classes are also loaded).
+C<ex::monkeypatched> injects methods when you C<use> it.  There are two ways
+to invoke it with C<use>: one is easy but inflexible, and the other is more
+flexible but also more awkward.
+
+In the easy form, your C<use> call should supply the name of a class to
+patch, and a listified hash from method names to code references
+implementing those methods:
+
+    use ex::monkeypatched 'Some::Class' => (
+        m1 => sub { ... },  # $x->m1 on Some::Class will now run this
+        m2 => sub { ... },  # $x->m2 on Some::Class will now run this
+    );
+
+In the flexible form, your C<use> call supplies a single hashref saying what
+methods to create.  That last example can be done exactly like this:
+
+    use ex::monkeypatched { class => 'Some::Class', methods => {
+        m1 => sub { ... },  # $x->m1 on Some::Class will now run this
+        m2 => sub { ... },  # $x->m2 on Some::Class will now run this
+    } };
+
+However, this flexible form also lets you add a method of a single name to
+several classes at once:
+
+    use ex::monkeypatched { method => 'm3', implementations => {
+        'Some::BaseClass'     => sub { ... },
+        'Some::Subclass::One' => sub { ... }
+        'Some::Subclass::Two' => sub { ... },
+    } };
+
+This is helpful when you want to provide a method for several related
+classes, with a different implementation in each of them.
+
+The classes to be patched will normally be loaded automatically before any
+patching is done (thus ensuring that all their base classes are also
+loaded).
+
+That doesn't work when you're trying to modify a class which can't be loaded
+directly; for example, the L<XML::LibXML> CPAN distribution provides a class
+named C<XML::LibXML::Node>, but trying to C<use XML::LibXML::Node> fails.
+In that situation, you can tell C<ex::monkeypatched> not to load the
+original class:
+
+    use ex::monkeypatched -norequire => 'XML::LibXML::Node' => (
+        clunk => sub { ... },
+        eth   => sub { ... },
+    );
+
+    # Equivalently:
+    use ex::monkeypatched -norequire => {
+        class   => 'XML::LibXML::Node',
+        methods => {
+            clunk => sub { ... },
+            eth   => sub { ... },
+        },
+    };
 
 Alternatively, you can inject methods after a class has already been loaded,
 using the C<inject> method:
 
     use ex::monkeypatched;
 
-    ex::monkeypatched->inject('Third::Party::Class' => (
+    ex::monkeypatched->inject('XML::LibXML::Node' => (
         clunk => sub { ... },
         eth   => sub { ... },
     );
 
-Calling C<inject> like this does not load the class in question, so
-C<ex::monkeypatched> is unable to guarantee that all the target class's
-methods have been loaded at the point the new methods are injected.
+    # Equivalently:
+    ex::monkeypatched->inject({ class => 'XML::LibXML::Node', methods => {
+        clunk => sub { ... },
+        eth   => sub { ... },
+    }});
+
+Neither of these approaches (C<-norequire> and C<inject>) loads the class in
+question, so when you use them, C<ex::monkeypatched> is unable to guarantee
+that all the target class's methods have been loaded at the point the new
+methods are injected.
 
 The C<ex::> prefix on the name of this module indicates that its API is
 still considered experimental.  However, the underlying code has been in use
--- t/basic.t
+++ t/basic.t
@@ -34,6 +34,52 @@ use Test::Exception;
         'Correctly refuse to override an inherited method';
 }
 
+{
+    no_class_ok('Monkey::D');
+    require_ok('Monkey::PatchD');
+    can_ok('Monkey::D', qw<monkey_d>);
+    throws_ok { 'Monkey::D'->new }
+        qr/^Can't locate object method "new" via package "Monkey::D"/,
+        '-norequire option does not load target package';
+    require_ok('Monkey::D');
+    my $obj = new_ok('Monkey::D', [], 'monkey-patched version');
+    can_ok($obj, qw<meth_d monkey_d>);
+}
+
+{
+    no_class_ok($_) for qw<Monkey::Sys Monkey::Sys::A Monkey::Sys::B Monkey::Sys::C>;
+    require_ok('Monkey::Sys');
+    can_ok('Monkey::Sys::A', 'sys_a_1');
+    lives_ok {
+        eval q{
+            use ex::monkeypatched -norequire => { method => 'foo', implementations => {
+                'Monkey::Sys::A' => sub { 'in Monkey::Sys::A foo' },
+                'Monkey::Sys::B' => sub { 'in Monkey::Sys::B foo' },
+            } };
+            1
+        } or die $@;
+    } 'name+implementations lives';
+    my $obj = new_ok('Monkey::Sys::B', [], 'monkey-patched version');
+    can_ok($obj, 'foo')
+        and is($obj->foo, 'in Monkey::Sys::B foo', 'name+implementations gets right method');
+}
+
+{
+    can_ok('Monkey::Sys::C', 'sys_c_1');
+    lives_ok {
+        eval q{
+            use ex::monkeypatched -norequire => { class => 'Monkey::Sys::C', methods => {
+                foo => sub { 'in Monkey::Sys::C foo' },
+                bar => sub { 'in Monkey::Sys::C bar' },
+            } };
+            1
+        } or die $@;
+    } 'class+methods lives';
+    my $obj = new_ok('Monkey::Sys::C', [], 'monkey-patched version');
+    can_ok($obj, 'foo')
+        and is($obj->foo, 'in Monkey::Sys::C foo', 'class+methods gets right method');
+}
+
 throws_ok { ex::monkeypatched->import('Monkey::False', f => sub {}) }
     qr{^Monkey/False\.pm did not return a true value},
     'Exception propagated from require for false module';
@@ -42,9 +88,9 @@ throws_ok { ex::monkeypatched->import('Monkey::Invalid', f => sub {}) }
     qr{^syntax error at .*Monkey/Invalid\.pm line },
     'Exception propagated from require for invalid module';
 
-throws_ok { ex::monkeypatched->import($_) } qr/^Invalid class name "\Q$_\E"/,
-    'Correctly refuse to load invalid class name "$_"'
-    for '', ' ', '!!', '2d6';
+throws_ok { eval q{use ex::monkeypatched 'Monkey::Q1', 'meth'; 1} or die $@ }
+    qr{^Usage: use ex::monkeypatched \$class => %methods},
+    'Argument validation: missing method body';
 
 done_testing();
 
